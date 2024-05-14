6e54aee248bd9cdec5613ee050d2c84d177af7aa
Oumayma Hani <oumayma.hani278@gmail.com>
2024-05-14 18:52:08 +0200

    add routers pages and components

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Error from './components/Error'
+import Header from './components/Header'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
