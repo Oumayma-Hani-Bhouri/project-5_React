c8e230ced9307b0af59f392db195a1438d5d3600
Oumayma Hani <oumayma.hani278@gmail.com>
2024-06-03 23:51:52 +0200

    add a favicon

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/home";
 import About from "./pages/About/about";
-import LogementCarousel from "./pages/House/logement";
+import Logementdetails from "./pages/House/logement";
 import Error from "./pages/error-page";
 import Header from "./components/Header/header";
 import Footer from "./components/Footer/footer";
@@ -15,7 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="logement/:houseId" element={<LogementCarousel />} />
+          <Route path="logement/:houseId" element={<Logementdetails />} />
           <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
